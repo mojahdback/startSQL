502887eaecb999a343754ff5bcec1a93d814cc91
mohamed <mojahdontheway@gmail.com>
2025-12-02 15:49:15 +0100

    add creation file for database and tables

---
+++ creation.sql
@@ -0,0 +1,115 @@
+
+-- Database
+CREATE DATABASE hospitalmanagement;
+
+
+
+-- TABLE: departments
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    location VARCHAR(100)
+);
+
+
+
+-- TABLE: doctors
+
+CREATE TABLE doctors (
+    doctor_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    specialization: VARCHAR(50),
+    phone_number VARCHAR(20),
+    email VARCHAR(100) UNIQUE,
+    department_id INT(11),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- TABLE: patients
+
+CREATE TABLE patients (
+    patient_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender ENUM('Male', 'Female','Other') NOT NULL,
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    address VARCHAR(255)
+);
+
+-- TABLE: rooms
+
+CREATE TABLE rooms (
+    room_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    room_number VARCHAR(10) NOT NULL UNIQUE,
+    room_type ENUM('General','Private','ICU') NOT NULL,
+    availability tinyint(1)
+);
+
+-- TABLE: admissions 
+
+CREATE TABLE admissions (
+    admission_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    patient_id INT(11),
+    room_id INT(11),
+    admission_date DATE,
+    discharge_date DATE,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+     FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+
+);
+
+-- TABLE: staff 
+CREATE TABLE staff (
+    staff_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    job_title VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    department_id INT(11),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- TABLE: appointments 
+
+CREATE TABLE appointments (
+    appointment_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    appointment_date DATE,
+    appointment_time TIME,
+    doctor_id INT(11),
+    patient_id INT(11),
+    reason VARCHAR(255),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+);
+
+
+-- TABLE: prescriptions
+
+CREATE TABLE prescriptions (
+    prescription_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    doctor_id INT(11),
+    patient_id INT(11),
+    medication_id INT(11),
+    prescription_date DATE,
+    dosage_instructions VARCHAR(255),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id),
+    FOREIGN KEY (medication_id) REFERENCES medications(medication_id)
+);
+
+-- TABLE: medications
+
+CREATE TABLE medications (
+    medication_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    medication_name VARCHAR(100),
+    dosage VARCHAR(50)
+);
+
+
+
+ 
+
