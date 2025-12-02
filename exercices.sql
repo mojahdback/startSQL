197e0817c5f67f70221d66a060894bea0037114f
mohamed <mojahdontheway@gmail.com>
2025-12-02 21:41:57 +0100

    add exercice 1: insert a patient in table patient

---
+++ exercices.sql
@@ -0,0 +1,4 @@
+-- Exercice 1 : CRUD Operations
+
+INSERT INTO patients(patient_id,first_name,last_name,gender,date_of_birth,phone_number)
+VALUES (6,'Alex','Johnson','Male','1990-07-15','1234567890');
