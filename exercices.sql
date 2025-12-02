aa1df96a3aa48e24eeb45ebadd82671f9d94b988
mohamed <mojahdontheway@gmail.com>
2025-12-02 22:02:54 +0100

    add exercice 2 : select all departments

--- exercices.sql
+++ exercices.sql
@@ -2,3 +2,7 @@
 
 INSERT INTO patients(patient_id,first_name,last_name,gender,date_of_birth,phone_number)
 VALUES (6,'Alex','Johnson','Male','1990-07-15','1234567890');
+
+-- Exercice 2 : SELECT Statement 
+
+SELECT * FROM departments;
