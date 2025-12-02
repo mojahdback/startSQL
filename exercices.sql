9439e8882859275b5c588d9f20281d28bb6a992c
mohamed <mojahdontheway@gmail.com>
2025-12-02 22:15:10 +0100

    add exercice 3 : sort the patients with date of born

--- exercices.sql
+++ exercices.sql
@@ -6,3 +6,8 @@ VALUES (6,'Alex','Johnson','Male','1990-07-15','1234567890');
 -- Exercice 2 : SELECT Statement 
 
 SELECT * FROM departments;
+
+-- Exercice 3 : ORDER BY Clause
+
+SELECT * FROM patients
+ORDER BY date_of_birth ASC;
